8c1302b729e2d13aa644990fd81fe70212e2fe1a
Alex Martinez <alexkmartinez77@gmail.com>
2021-12-16 14:55:57 -0800

    Creates datasources.production.js file.

---
+++ server/datasources.production.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "MongoDB": {
+      "name": "MongoDB",
+      "connector": "mongodb",
+      "url": `mongodb+srv://alexkmartinez77:process.env.${MONGODB_URI}@loopbackWorkshop.j9ckx.mongodb.net/myFirstDatabase?retryWrites=true`
+  }
+}
+
