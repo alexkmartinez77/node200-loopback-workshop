f60d17404d3a76d88bd489fbd4dffd36623c3a66
Alex Martinez <alexkmartinez77@gmail.com>
2021-12-16 09:00:38 -0800

    Adds config.development.js and config.production.jsfiles.

---
+++ server/config.production.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "restApiRoot": "/api",
+  "host": process.env.HOST,
+  "port": process.env.PORT,
+  "remoting": {
+    "context": false,
+    "rest": {
+      "handleErrors": false,
+      "normalizeHttpPath": false,
+      "xml": false
+    },
+    "json": {
+      "strict": false,
+      "limit": "100kb"
+    },
+    "urlencoded": {
+      "extended": true,
+      "limit": "100kb"
+    },
+    "cors": false
+  }
+}
