5808f2b78ba961439648ed1a14d19d0cd6301de4
Alex Martinez <alexkmartinez77@gmail.com>
2021-12-16 15:38:17 -0800

    CircleCI

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,6 +1,7 @@
 module.exports = {
-  "MongoDB": {
-      "name": "MongoDB",
+  "db": {
+      "port": 27017,
+      "name": "mongoDB",
       "connector": "mongodb",
       "url": `mongodb+srv://alexkmartinez77:${process.env.MONGODB_URI}@sdcsblogapi.j9ckx.mongodb.net/loopbackWorkshop?retryWrites=true`
   }
