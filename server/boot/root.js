56fd01798b2ecaa8a31a04cf9643f5426ba47bbe
Alex Martinez <alexkmartinez77@gmail.com>
2021-12-16 14:19:05 -0800

    Comments out default status in root.js and creates index.html and styles.css to server at root /

--- server/boot/root.js
+++ server/boot/root.js
@@ -8,6 +8,7 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   const router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  //by default loopback serves the status invoked be method below
+  //router.get('/', server.loopback.status());
   server.use(router);
 };
