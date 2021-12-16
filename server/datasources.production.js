c36c208c0de972aface4269173846ff6ba6a7315
Alex Martinez <alexkmartinez77@gmail.com>
2021-12-16 15:21:40 -0800

    Updates datasources.production.js

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,7 +2,6 @@ module.exports = {
   "MongoDB": {
       "name": "MongoDB",
       "connector": "mongodb",
-      "url": `mongodb+srv://alexkmartinez77:${process.env.MONGODB_URI}@loopbackWorkshop.j9ckx.mongodb.net/myFirstDatabase?retryWrites=true`
+      "url": `mongodb+srv://alexkmartinez77:${process.env.MONGODB_URI}@sdcsblogapi.j9ckx.mongodb.net/loopbackWorkshop?retryWrites=true`
   }
 }
-
